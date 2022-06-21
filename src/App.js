7cea5a739b32d31409dec6cd3d15088be92554e6
AlexMuia31 <alexmuia31@gmail.com>
2022-06-21 22:00:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Button, TextField } from "@mui/material";
+import React from "react";
 
 function App() {
+  const [textFields, setTextFields] = React.useState(0);
+  const handleTextField = () => {
+    setTextFields(textFields + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <TextField />
+      <Button onClick={handleTextField}>ADD TEXT Field</Button>
+      {[...Array(textFields).keys()].map(() => (
+        <TextField />
+      ))}
+    </Box>
   );
 }
 
